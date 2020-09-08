a7e6d822b0917b27465fc6aa66b5fc1dc4dad2bc
Himank Batra <himank2010@gmail.com>
2020-09-09 03:03:59 +0530

    update readme

--- docker-setup.sh
+++ docker-setup.sh
@@ -3,19 +3,19 @@ set -e
 
 echo "Building animal-name-service docker image..."
 cd animal-name-service
-mvn clean install
+./mvnw clean install
 docker build -t com.example/animal-name-service:0.1.0 .
 echo "Built animal-name-service docker image..."
 
 echo "Building name-generator-service docker image..."
 cd ../name-generator-service
-mvn clean install
+./mvnw clean install
 docker build -t com.example/name-generator-service:0.1.0 .
 echo "Built name-generator-service docker image..."
 
 echo "Building scientist-name-service docker image..."
 cd ../scientist-name-service
-mvn clean install
+./mvnw clean install
 docker build -t com.example/scientist-name-service:0.1.0 .
 echo "Built scientist-name-servicedocker image..."
 
